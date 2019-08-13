3812c024fec6f7db29c8b04888d4b63aea173c29
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-08-14 00:54:19 +0700

    mengurutkan Abjad

---
+++ mengurutkanAbjad.js
@@ -0,0 +1,30 @@
+function urutkanAbjad(str) {
+  var abjad = "abcdefghijklmnopqrstuvwqyz";
+  var angka = [];
+  for (var i = 0; i < str.length; i++) {
+    angka.push(abjad.indexOf(str[i]));
+  }
+  var flag = false;
+  while (flag === false) {
+    flag = true;
+    for (var j = 0; j < angka.length; j++) {
+      if (angka[j - 1] > angka[j]) {
+        var tampung = angka[j - 1];
+        angka[j - 1] = angka[j];
+        angka[j] = tampung;
+      }
+    }
+  }
+  var kata = "";
+  for (var i = 0; i < angka.length; i++) {
+    kata += abjad[angka[i]];
+  }
+  return kata;
+}
+
+// TEST CASES
+console.log(urutkanAbjad("hello")); // 'ehllo'
+console.log(urutkanAbjad("truncate")); // 'acenrttu'
+console.log(urutkanAbjad("developer")); // 'deeeloprv'
+console.log(urutkanAbjad("software")); // 'aeforstw'
+console.log(urutkanAbjad("aegis")); // 'aegis'
