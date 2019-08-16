b99eb06b5c54f33c38324ac177b6a824d56107ce
GustiPutraJarmana <gustimertaputra@gmail.com>
2019-08-16 14:36:56 +0700

    Naik angkot

---
+++ naikAngkot.js
@@ -0,0 +1,27 @@
+function naikAngkot(arrPenumpang) {
+  var rute = ["A", "B", "C", "D", "E", "F"];
+  if (!arrPenumpang) {
+    return [];
+  }
+  var output = [];
+  for (var i = 0; i < arrPenumpang.length; i++) {
+    var haragPerSport = 2000;
+    var jarak =
+      rute.indexOf(arrPenumpang[i][2]) - rute.indexOf(arrPenumpang[i][1]);
+    var Angkotpenumpang = {
+      penumpang: arrPenumpang[i][0],
+      naikDari: arrPenumpang[i][1],
+      tujuan: arrPenumpang[i][2],
+      bayar: jarak * haragPerSport
+    };
+    output.push(Angkotpenumpang);
+  }
+  return output;
+}
+
+//TEST CASE
+console.log(naikAngkot([["Dimitri", "B", "F"], ["Icha", "A", "B"]]));
+// [ { penumpang: 'Dimitri', naikDari: 'B', tujuan: 'F', bayar: 8000 },
+//   { penumpang: 'Icha', naikDari: 'A', tujuan: 'B', bayar: 2000 } ]
+
+console.log(naikAngkot([])); //[]
